d3f7cfdf9de3b00d821b91351001dadbae97b960
leombpunk <leomb_punk@hotmail.com>
2022-12-17 22:28:24 -0300

    conexion a mysql

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { dbConnectMySQL } = require("./config/mysql");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("storage")); //le dice a express cual es el contenido estatico y por ende publico de la aplicacion, para poder acceder desde el navegador http://localhost:puerto/nombredelarchivo
 const port = process.env.PORT || 3000;
 
-app.use("/api", require('./routes')); //invoca al archivo index del directorio routes
+app.use("/api", require("./routes")); //invoca al archivo index del directorio routes
 
 app.listen(port, () => {
-    console.log("puto el que escuche el puerto: " + port);
+  console.log("puto el que escuche el puerto: " + port);
 });
+
+//conexion a MySQL
+dbConnectMySQL();
